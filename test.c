cab6536569eff1c5c64efb152f5875c03b24589b
MHS <gek@katherine>
2021-04-10 12:09:54 -0400

    Automatic commit.

--- test.c
+++ test.c
@@ -17,7 +17,7 @@ int main(int argc, char** argv){
 	puts("\nReading from stdin until terminator B or 9 characters...\n");
 	char* buf = malloc(10);
 	if(!buf)return 1;
-	unsigned long long blen = 0; blen = read_until_terminator(stdin, buf, 10, 'B'); fgetc(stdin); //Used to handle erroneous '\n grabs.'
+	unsigned long blen = 0; blen = read_until_terminator(stdin, buf, 10, 'B'); fgetc(stdin); //Used to handle erroneous '\n grabs.'
 	printf("\nYou entered a string of length %u:\n%s\n",blen, buf);
 	free(buf); buf = NULL;
 	//Test read_until_terminator_alloc
@@ -34,7 +34,7 @@ int main(int argc, char** argv){
 
 	FILE* f = fopen("README.MD", "rb");
 	if(!f) return 1;
-	unsigned long long len;
+	unsigned long len;
 	buf = NULL; buf = read_file_into_alloced_buffer(f, &len);
 	if(buf){
 		printf("\nThe entire contents of README.MD are:\n%s\n~~~\n",buf);
@@ -44,9 +44,9 @@ int main(int argc, char** argv){
 
 	//Substring search.
 	const char* text = "Oh, what a mighty fine day it is!";
-	long long pos = strfind(text, "mighty"); 
-	long long pos2 = strfind(text, "Mighty"); //-1 means it could not find it
-	long long pos3 = strfind(text, ""); 
+	long pos = strfind(text, "mighty"); 
+	long pos2 = strfind(text, "Mighty"); //-1 means it could not find it
+	long pos3 = strfind(text, ""); 
 	printf("\n\"mighty\" is at %d\n", pos);
 	printf("\nwhich is:%s\n", text+pos);
 	printf("\"Mighty\" is at %d\n", pos2);
