f5c0aa3a6a8c4e63f39065b66a084aff96d5243e
MHS <gek@katherine>
2021-04-24 01:50:43 -0400

    Automatic commit.

--- test.c
+++ test.c
@@ -18,14 +18,14 @@ int main(int argc, char** argv){
 	char* buf = malloc(10);
 	if(!buf)return 1;
 	unsigned long blen = 0; blen = read_until_terminator(stdin, buf, 10, 'B'); fgetc(stdin); //Used to handle erroneous '\n grabs.'
-	printf("\nYou entered a string of length %u:\n%s\n",blen, buf);
+	printf("\nYou entered a string of length %lu:\n%s\n",blen, buf);
 	free(buf); buf = NULL;
 	//Test read_until_terminator_alloc
 	puts("\nReading from stdin until terminator Q with infinite capacity...\n");
 	blen = 0;
 	buf = read_until_terminator_alloced(stdin, &blen, 'Q',8);
 	if(buf){
-		printf("\nYou entered a string of length %u:\n%s\n",blen, buf); fgetc(stdin); //Used to handle erroneous '\n grabs.'
+		printf("\nYou entered a string of length %lu:\n%s\n",blen, buf); fgetc(stdin); //Used to handle erroneous '\n grabs.'
 		free(buf); buf = NULL;
 	}else {
 		puts("\nSome sort of error?\n");
@@ -43,14 +43,17 @@ int main(int argc, char** argv){
 	if(f) fclose(f);
 
 	//Substring search.
-	const char* text = "Oh, what a mighty fine day it is!";
+	const char* text = "Oh, what a mighty fine day it is! cccars";
 	long pos = strfind(text, "mighty"); 
 	long pos2 = strfind(text, "Mighty"); //-1 means it could not find it
+	long pos4 = strfind(text, "ccars");
 	long pos3 = strfind(text, ""); 
-	printf("\n\"mighty\" is at %d\n", pos);
+	printf("\n\"mighty\" is at %ld \n", pos);
 	printf("\nwhich is:%s\n", text+pos);
-	printf("\"Mighty\" is at %d\n", pos2);
-	printf("\"Oh,\" is at %d\n", strfind(text, "Oh,"));
-	printf("\"\" is at %d\n", pos3);
+	printf("\"Mighty\" is at %ld \n", pos2);
+	printf("\"Oh,\" is at %ld\n", strfind(text, "Oh,"));
+	printf("\"\" is at %ld\n", pos3);
+	printf("\"ccars\" is at %ld\n", pos4);
+	printf("\nwhich is:%s\n", text+pos4);
 	return 0;
 }
