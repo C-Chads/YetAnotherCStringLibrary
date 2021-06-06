06a3390a0bd9727919b0be37c7463783ce34debc
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-06-06 03:07:34 -0400

    Automatic commit.

--- functional.h
+++ functional.h
@@ -10,10 +10,10 @@ Loops with bounded execution
 */
 
 #define loop(v, e)\
-for(unsigned long v = 0, __internal_##v = 0; __internal_##v < e; __internal_##v++, v = __internal_##v)
+for(unsigned long v = 0, __geks_internal_##v = 0; __geks_internal_##v < e; __geks_internal_##v++, v = __geks_internal_##v)
 
 #define loople(v, e)\
-for(unsigned long v = 0, __internal_##v = 0; __internal_##v <= e; __internal_##v++, v = __internal_##v)
+for(unsigned long v = 0, __geks_internal_##v = 0; __geks_internal_##v <= e; __geks_internal_##v++, v = __geks_internal_##v)
 
 
 
